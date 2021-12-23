1fd049cdf4fa092efcd90488c1bd1763547ce791
nerdyKiam <45430833+nerdyKiam@users.noreply.github.com>
2021-12-23 20:41:13 +0600

    Kiam Final Touch

--- src/Components/Mynetwork/MyNetwork.js
+++ src/Components/Mynetwork/MyNetwork.js
@@ -5,9 +5,7 @@ import '../ScaffoldLayoutMiddle/ScaffoldLayoutMiddle.css'
 const MyNetwork = (props) => {
 
     const {user} = useContext(AuthContext)
-    const [data, setData] = useState([]);
-    const [searchData, setSearchData] = useState([]);
-    const [connectRequest,setConnectRequest] = useState(false);
+    const [data, setData] = useState([])
 
     // fetching data from server
     useEffect(() => {
@@ -18,12 +16,10 @@ const MyNetwork = (props) => {
                 // filtering user himself to show only other users
                 const filteredData = data.filter(x => x.email !== user.email)
                 setData(filteredData)
-
-                setSearchData(filteredData)
-
-                console.log(searchData)
             })
     }, [])
+
+    const [a, setA] = useState(false)
 
 
     return (
@@ -35,21 +31,22 @@ const MyNetwork = (props) => {
                     </div>
                     <div class="col border mx-2">
                         {
-                            searchData.map(x =>
+                            data.map(x =>
                                 <div className='d-flex align-items-start mb-3'>
                                     <img className='img-right-section' src={x.photoURL}/>
                                     <div className='data-follow d-flex flex-column '>
                                         <span className='title-follow'>{x.displayName}</span>
                                         <span className='bio-follow'>CS Student | Programmer</span>
                                         {
-                                            connectRequest==='true'?
-                                            <button type="button"onClick={setConnectRequest(false)}
-                                                    className="follow-button-right-section btn btn-outline-secondary d-flex align-items-center justify-content-center">
-                                                <span>+ Connect</span>
-                                            </button>:<button type="button" onClick={setConnectRequest(true)}
-                                                    className="follow-button-right-section btn btn-outline-secondary d-flex align-items-center justify-content-center">
-                                                <span>Request</span>
-                                            </button>
+                                            a == true ?
+                                                <button type="button" onClick={()=>{setA(false)}}
+                                                        className="follow-button-right-section btn btn-outline-secondary d-flex align-items-center justify-content-center">
+                                                    <span>+ Connect</span>
+                                                </button> :
+                                                <button type="button" onClick={()=>{setA(true)}}
+                                                        className="follow-button-right-section btn btn-outline-secondary d-flex align-items-center justify-content-center">
+                                                    <span>Requested</span>
+                                                </button>
                                         }
                                     </div>
                                 </div>
